09b77bf0295557b388dbe71544515117a853f6b2
KyleUmney <l.il.kyle@hotmail.co.uk>
2018-09-03 21:54:10 +0100

    Test

--- DoingAThing/Sprites/Player.cs
+++ DoingAThing/Sprites/Player.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace DoingAThing.Sprites
 {
   class Player : Sprite
   {
+    public Player(Texture2D texture) : base(texture)
+    {
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
